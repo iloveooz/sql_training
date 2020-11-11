26f2851e16de5b8196b6d967927355951c84b659
iloveooz <iloveooz@yandex.ru>
2020-11-12 00:35:57 +0700

    57 step ended

--- training.sql
+++ training.sql
@@ -1172,3 +1172,10 @@ Affected rows: 0
 */
 
 -- 57. 
+insert into author(name_author) values ('Булгаков М.А.');
+insert into author(name_author) values ('Достоевский Ф.М.');
+insert into author(name_author) values ('Есенин С.А.');
+insert into author(name_author) values ('Пастернак Б.Л.');
+select * from author;
+
+-- 58. 
